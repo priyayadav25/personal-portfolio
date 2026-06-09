f9c14f3870644567b94fc67a3c0b7fef373c97d8
priyayadav25 <priyay2519@gmail.com>
2026-06-09 13:51:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
+const projects = [
+  {
+    id: 1,
+    title: "Personal Portfolio",
+    technology: "HTML, CSS, JavaScript"
+  }
+];
+
 app.get("/", (req, res) => {
-    res.send("Portfolio Backend Working");
+  res.send("Portfolio Backend Running");
+});
+
+app.get("/projects", (req, res) => {
+  res.json(projects);
 });
 
 app.listen(5000, () => {
-    console.log("Server Running on Port 5000");
+  console.log("Server Running on Port 5000");
 });
