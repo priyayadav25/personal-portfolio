a0ed6cce94f1b1165feb26d13a15b324757dad56
priyayadav25 <priyay2519@gmail.com>
2026-06-09 14:28:39 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -2,6 +2,15 @@ const projects = [
   {
     id: 1,
     title: "Personal Portfolio Website",
-    technology: "HTML, CSS, JavaScript, Node.js"
+    technology: "HTML, CSS, JavaScript, Node.js",
+    description: "Full Stack Portfolio Website"
+  },
+  {
+    id: 2,
+    title: "Student Management System",
+    technology: "Java, MySQL",
+    description: "Manage student records"
   }
 ];
+
+module.exports = projects;
